0ffdd64d91b420b7d744abadd9cad417f9020bb1
tomoki-yamamura <progtab17@gmail.com>
2025-01-19 21:50:15 +0900

    default

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+        <a href="/about">normal link -- about page</a>
+      </nav>
+    </header>
+    
     <h2>About</h2>
     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam natus at
     doloremque qui, adipisci eligendi nisi culpa, rerum illum molestias
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <header>
+      <nav>
+        <NuxtLink to="/">Nuxt Dojo</NuxtLink>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+        <a href="/about">normal link -- about page</a>
+      </nav>
+    </header>
     <h2>Home</h2>
     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam natus at
     doloremque qui, adipisci eligendi nisi culpa, rerum illum molestias
@@ -10,11 +21,11 @@
 <script setup></script>
 
 <style scoped>
-  h2 {
-    margin-bottom: 20px;
-    font-size: 36px;
-  }
-  p {
-    margin: 20px 0;
-  }
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
 </style>
