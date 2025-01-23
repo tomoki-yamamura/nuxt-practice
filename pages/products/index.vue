5bc5cbb7a0671a533aa7fd5423377bd7e405f777
tomoki-yamamura <progtab17@gmail.com>
2025-01-24 00:14:09 +0900

    fetch

---
+++ pages/products/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div v-for="p in products">
+      <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+definePageMeta({
+  layout: "products",
+});
+const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
